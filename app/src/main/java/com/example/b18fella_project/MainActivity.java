16146db3f084491dbc7a625c30fbabf34c4001f4
b18fella <b18fella@student.his.se>
2019-05-16 16:22:51 +0200

    Refresh implememted.

--- app/src/main/java/com/example/b18fella_project/MainActivity.java
+++ app/src/main/java/com/example/b18fella_project/MainActivity.java
@@ -77,6 +77,10 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.refresh_data) {
+            adapter.clear();
+            new FetchData().execute();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
